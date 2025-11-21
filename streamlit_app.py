6379642447ec29c7de550a39854cb6144360c555
yliaandrusenko-alt <ylia.andrusenko@gmail.com>
2025-11-21 16:51:18 +0100

    Update streamlit_app.py
    Added df

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,6 +47,6 @@ if time_to_insert:
 
 # New section to display smoothiefroot nutrition information
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+# st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
