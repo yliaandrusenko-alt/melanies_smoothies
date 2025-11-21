472d33990372127f4c33131baac75563ea248bc0
yliaandrusenko-alt <ylia.andrusenko@gmail.com>
2025-11-21 16:45:03 +0100

    Update streamlit_app.py
    Added new source for smoothies

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,3 +44,8 @@ time_to_insert = st.button('Submit Order')
 if time_to_insert:
     session.sql(my_insert_stmt).collect()
     st.success('Your Smoothie is ordered, '+ name_on_order +"!", icon="✅")
+
+# New section to display smoothiefroot nutrition information
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
