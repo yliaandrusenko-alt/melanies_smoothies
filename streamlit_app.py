2ae91d4b95d115472be59e9bea52e61d30e43806
yliaandrusenko-alt <ylia.andrusenko@gmail.com>
2025-11-21 17:01:15 +0100

    Update streamlit_app.py
    Bug fixing

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,13 +32,12 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' '
     
     #st.write(ingredients_string) 
+    
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+        values ('""" + ingredients_string + """','""" + name_on_order + """')"""
+    
+    st.write(my_insert_stmt)
 
-
-my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-    values ('""" + ingredients_string + """','""" + name_on_order + """')"""
-
-st.write(my_insert_stmt)
-#st.stop()
 time_to_insert = st.button('Submit Order')
 
 if time_to_insert:
